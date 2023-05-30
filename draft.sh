84bb5dda78798a7d65655785a805ed62d14acdfa
oxu2 <oxu@utdallas.edu>
2023-05-29 20:39:54 -0500

    test

---
+++ draft.sh
@@ -0,0 +1,3 @@
+cd ~/PyCIL; nohup python main.py --config=./exps/icarl.json &
+cd ~/PyCIL; nohup python main.py --config=./exps/gem.json &
+cd ~/PyCIL; nohup python main.py --config=./exps/finetune.json &
